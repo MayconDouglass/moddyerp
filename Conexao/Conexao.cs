c67a462ff8d139c4c303eb5fb1a87b0d66bb51e0
Maycon Douglas <mdouglastr@gmail.com>
2021-09-25 16:38:44 -0300

    Adição do Drive MySQL

---
+++ Conexao/Conexao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Conexao
+{
+    class Conexao
+    {
+        //Configuração Base de Desenvolvimento Mysql
+        static string server = "erpmoddy.mysql.uhserver.com";
+        static string port = "3306";
+        static string userName = "moddy";
+        static string password = "159753m@";
+        static string database = "erpmoddy";  // Banco de dados
+
+        public static String ConBD
+        {
+            get
+            {
+                return String.Format("Server={0};Port={1};User={2};Password={3};Database={4};", server, port, userName, password, database);
+            }
+        }
+    }
+}
---
+++ Model/UsuarioModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Model
+{
+    class UsuarioModel
+    {
+        private int id_usuario;
+        private int idPerfil;
+        private string login;
+        private string nome;
+        private string sobrenome;
+        private string password;
+        private int status;
+        private DateTime data_cad;
+        private DateTime data_alt;
+
+        public int Id_usuario { get => id_usuario; set => id_usuario = value; }
+        public int IdPerfil { get => idPerfil; set => idPerfil = value; }
+        public string Login { get => login; set => login = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Sobrenome { get => sobrenome; set => sobrenome = value; }
+        public string Password { get => password; set => password = value; }
+        public int Status { get => status; set => status = value; }
+        public DateTime Data_cad { get => data_cad; set => data_cad = value; }
+        public DateTime Data_alt { get => data_alt; set => data_alt = value; }
+    }
+}
